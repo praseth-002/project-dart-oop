961538231ce1fa73d4604324fb266735cfb75ca0
rong <darong6126@gmail.com>
2025-10-28 21:01:43 +0700

    fix: change staff attribute to private, feat: add UUID and nurse class

--- lib/domain/staff.dart
+++ lib/domain/staff.dart
@@ -1,20 +1,34 @@
+import 'package:uuid/uuid.dart';
 enum Role {Doctor, Nurse, Receptionist}
 enum Specialization {General, Pediatrician, Surgeon}
 
+var uuid = Uuid();
 class Staff {
-  String staffId;
-  String staffName;
-  String gender;
-  String dob;
-  Role position;
-  double baseSalary;
+  final String _staffId;
+  String _staffName;
+  String _gender;
+  String _dob;
+  Role _position;
+  double _baseSalary;
   // shift idk how to implement yet TBC
 
-  Staff(this.staffId, this.staffName, this.gender, this.dob, this.position, this.baseSalary);
+  Staff (this._staffName, this._gender, this._dob, this._position, this._baseSalary) : _staffId = uuid.v4();
+
+  //Getter function
+  String get staffId => _staffId;
+  String get staffName => _staffName;
+  String get gender => _gender;
+  String get dob => _dob;
+  Role get position => _position;
+  double get baseSalary => _baseSalary;
+
+  //Setter 
+  set baseSalary(double value) => _baseSalary = value;
+
 
   @override  
   String toString() {
-    return "$staffId $staffName $gender $dob $position $baseSalary";
+    return " staffId:$_staffId\nname:$_staffName\ngender:$_gender\ndob:$_dob\nposition:$_position\nbaseSalary:$_baseSalary";
   }
 }
 
@@ -28,5 +42,19 @@ class Doctor extends Staff {
     String dob,
     double baseSalary,
     this.specialization,
-  ) : super(staffId, staffName, gender, dob, Role.Doctor, baseSalary);
+  ) : super(staffName, gender, dob, Role.Doctor, baseSalary);
+}
+
+class Nurse extends Staff {
+  List<String> certification;
+  int yearOfExperince;
+
+  Nurse(
+    String staffId,
+    String staffName,
+    String gender,
+    String dob,
+    double baseSalary,
+    this.certification, 
+    this.yearOfExperince) : super (staffName, gender, dob, Role.Nurse, baseSalary);
 }
