a6c56491fa4976bb03a3db6f092b1454af860ad1
praseth002 <prasethsokha@gmail.com>
2025-11-07 11:53:35 +0700

    fix: no more dynamic

--- lib/domain/attendance.dart
+++ lib/domain/attendance.dart
@@ -6,44 +6,13 @@ class Attendance {
 
   Duration timeWorked() => end.difference(start);
 
-  Map<String, dynamic> toJson() => {
+  Map<String, Object> toJson() => {
         'start': start.toIso8601String(),
         'end': end.toIso8601String(),
       };
 
-  factory Attendance.fromJson(Map<String, dynamic> json) => Attendance(
-        DateTime.parse(json['start']),
-        DateTime.parse(json['end']),
+  factory Attendance.fromJson(Map<String, Object> json) => Attendance(
+        DateTime.parse(json['start'] as String),
+        DateTime.parse(json['end'] as String),
       );
 }
-
-
-// class Attendance {
-//   DateTime shiftStart;
-//   DateTime shiftEnd;
-
-//   Attendance (this.shiftStart, this.shiftEnd);
-
-//   Duration timeWorked() {
-//     return shiftEnd.difference(shiftStart);
-//   } 
-
-//   @override  
-//   String toString() {
-//     final duration = timeWorked();
-//     final hours = duration.inHours;
-//     final minutes = duration.inMinutes.remainder(60);
-//     return "Shift Start: $shiftStart, Shift End: $shiftEnd, Worked For: ${hours}h ${minutes}m";
-//   }
-// }
-
-// void main() {
-//   var att = Attendance(
-//     DateTime(2025, 11, 2, 8, 0),
-//     DateTime(2025, 11, 2, 17, 30),
-//   );
-
-//   print(att.timeWorked().inHours); // 9
-//   print(att.timeWorked().inMinutes); // 570
-//   print(att);
-// }
--- lib/domain/staff.dart
+++ lib/domain/staff.dart
@@ -22,7 +22,8 @@ class Staff {
     this._position,
     this._baseSalary,
   ) : _staffId = uuid.v4();
-  // constructor for read staff from file
+
+  // constructor for loading from JSON file
   Staff._fromData(
     this._staffId,
     this._staffName,
@@ -32,7 +33,7 @@ class Staff {
     this._baseSalary,
   );
 
-  // Getters & Setters
+  // Getters & setters
   String get staffId => _staffId;
   String get staffName => _staffName;
   String get gender => _gender;
@@ -61,7 +62,7 @@ class Staff {
   String toString() => toJson().toString();
 
   // JSON serialization
-  Map<String, dynamic> toJson() => {
+  Map<String, Object> toJson() => {
         'type': 'Staff',
         'staffId': _staffId,
         'staffName': _staffName,
@@ -72,19 +73,21 @@ class Staff {
         'attendance': _attendance.map((a) => a.toJson()).toList(),
       };
 
-  factory Staff.fromJson(Map<String, dynamic> json) {
+  factory Staff.fromJson(Map<String, Object> json) {
     var staff = Staff._fromData(
-      json['staffId'],
-      json['staffName'],
-      json['gender'],
-      json['dob'],
-      Role.values.firstWhere((r) => r.name == json['position']),
+      json['staffId'] as String,
+      json['staffName'] as String,
+      json['gender'] as String,
+      json['dob'] as String,
+      Role.values.firstWhere((r) => r.name == (json['position'] as String)),
       (json['baseSalary'] as num).toDouble(),
     );
 
     if (json['attendance'] != null) {
       staff.attendance.addAll(
-          (json['attendance'] as List).map((a) => Attendance.fromJson(a)));
+          ((json['attendance'] as List<Object>)
+                  .map((a) => Attendance.fromJson(a as Map<String, Object>)))
+              .toList());
     }
 
     return staff;
@@ -112,7 +115,6 @@ class Doctor extends Staff {
     this.specialization,
   ) : super._fromData(staffId, staffName, gender, dob, Role.Doctor, baseSalary);
 
-
   @override
   void printInfo() {
     super.printInfo();
@@ -120,26 +122,29 @@ class Doctor extends Staff {
   }
 
   @override
-  Map<String, dynamic> toJson() {
+  Map<String, Object> toJson() {
     var data = super.toJson();
     data['type'] = 'Doctor';
     data['specialization'] = specialization.name;
     return data;
   }
 
-  factory Doctor.fromJson(Map<String, dynamic> json) {
+  factory Doctor.fromJson(Map<String, Object> json) {
     var doc = Doctor._fromData(
-      json['staffId'],
-      json['staffName'],
-      json['gender'],
-      json['dob'],
+      json['staffId'] as String,
+      json['staffName'] as String,
+      json['gender'] as String,
+      json['dob'] as String,
       (json['baseSalary'] as num).toDouble(),
-      Specialization.values.firstWhere((s) => s.name == json['specialization']),
+      Specialization.values
+          .firstWhere((s) => s.name == (json['specialization'] as String)),
     );
 
     if (json['attendance'] != null) {
       doc.attendance.addAll(
-          (json['attendance'] as List).map((a) => Attendance.fromJson(a)));
+          ((json['attendance'] as List<Object>)
+                  .map((a) => Attendance.fromJson(a as Map<String, Object>)))
+              .toList());
     }
 
     return doc;
@@ -174,12 +179,13 @@ class Nurse extends Staff {
   @override
   void printInfo() {
     super.printInfo();
-    print('Certifications: ${certification.isEmpty ? "None" : certification.join(", ")}');
+    print(
+        'Certifications: ${certification.isEmpty ? "None" : certification.join(", ")}');
     print('Years of Experience: $yearOfExperince');
   }
 
   @override
-  Map<String, dynamic> toJson() {
+  Map<String, Object> toJson() {
     var data = super.toJson();
     data['type'] = 'Nurse';
     data['certification'] = certification;
@@ -187,21 +193,26 @@ class Nurse extends Staff {
     return data;
   }
 
-  factory Nurse.fromJson(Map<String, dynamic> json) {
+  factory Nurse.fromJson(Map<String, Object> json) {
     var nurse = Nurse._fromData(
-      json['staffId'],
-      json['staffName'],
-      json['gender'],
-      json['dob'],
+      json['staffId'] as String,
+      json['staffName'] as String,
+      json['gender'] as String,
+      json['dob'] as String,
       (json['baseSalary'] as num).toDouble(),
-      yearOfExperince: json['yearOfExperince'] ?? 0,
+      yearOfExperince: json['yearOfExperince'] != null
+          ? (json['yearOfExperince'] as num).toInt()
+          : 0,
     );
 
-    nurse.certification = List<String>.from(json['certification'] ?? []);
+    nurse.certification =
+        (json['certification'] as List<Object>? ?? []).cast<String>();
 
     if (json['attendance'] != null) {
       nurse.attendance.addAll(
-          (json['attendance'] as List).map((a) => Attendance.fromJson(a)));
+          ((json['attendance'] as List<Object>)
+                  .map((a) => Attendance.fromJson(a as Map<String, Object>)))
+              .toList());
     }
 
     return nurse;
@@ -242,7 +253,7 @@ class Admin extends Staff {
   }
 
   @override
-  Map<String, dynamic> toJson() {
+  Map<String, Object> toJson() {
     var data = super.toJson();
     data['type'] = 'Admin';
     data['email'] = email;
@@ -250,20 +261,22 @@ class Admin extends Staff {
     return data;
   }
 
-  factory Admin.fromJson(Map<String, dynamic> json) {
+  factory Admin.fromJson(Map<String, Object> json) {
     var admin = Admin._fromData(
-      json['staffId'],
-      json['staffName'],
-      json['gender'],
-      json['dob'],
+      json['staffId'] as String,
+      json['staffName'] as String,
+      json['gender'] as String,
+      json['dob'] as String,
       (json['baseSalary'] as num).toDouble(),
-      json['email'],
-      json['password'],
+      json['email'] as String,
+      json['password'] as String,
     );
 
     if (json['attendance'] != null) {
       admin.attendance.addAll(
-          (json['attendance'] as List).map((a) => Attendance.fromJson(a)));
+          ((json['attendance'] as List<Object>)
+                  .map((a) => Attendance.fromJson(a as Map<String, Object>)))
+              .toList());
     }
 
     return admin;
@@ -271,111 +284,3 @@ class Admin extends Staff {
 }
 
 
-// import 'package:hospital_management/domain/attendance.dart';
-// import 'package:uuid/uuid.dart';
-// enum Role {Doctor, Nurse, Receptionist, Admin}
-// enum Specialization {General, Pediatrician, Surgeon}
-
-// var uuid = Uuid();
-// class Staff {
-//   final String _staffId;
-//   String _staffName;
-//   String _gender;
-//   String _dob;
-//   Role _position;
-//   double _baseSalary;
-//   final List<Attendance> _attendance = [];
-
-//   Staff (this._staffName, this._gender, this._dob, this._position, this._baseSalary) : _staffId = uuid.v4();
-
-//   //Getter function
-//   String get staffId => _staffId;
-//   String get staffName => _staffName;
-//   String get gender => _gender;
-//   String get dob => _dob;
-//   Role get position => _position;
-//   double get baseSalary => _baseSalary;
-//   List<Attendance> get attendance => _attendance;
-
-//   //Setter 
-//   set staffName(String name) => _staffName = name;
-//   set gender(String g) => _gender = g;
-//   set dob(String d) => _dob = d;
-//   set position(Role p) => _position = p;
-//   set baseSalary(double value) => _baseSalary = value;
-
-//   void printInfo() {
-//     print('------------------------------------------');
-//     print('ID: $_staffId');
-//     print('Name: $_staffName');
-//     print('Gender: $_gender');
-//     print('DOB: $_dob');
-//     print('Role: ${_position.name}');
-//     print('Base Salary: \$${_baseSalary.toStringAsFixed(2)}');
-//   }
-
-//   @override  
-//   String toString() {
-//     return " staffId:$_staffId\nname:$_staffName\ngender:$_gender\ndob:$_dob\nposition:$_position\nbaseSalary:$_baseSalary\nattendance:$_attendance";
-//   }
-// }
-
-// class Doctor extends Staff {
-//   Specialization specialization;
-
-//   Doctor(
-//     String staffName,
-//     String gender,
-//     String dob,
-//     double baseSalary,
-//     this.specialization,
-//   ) : super(staffName, gender, dob, Role.Doctor, baseSalary);
-
-//   @override
-//   void printInfo() {
-//     super.printInfo();
-//     print('Specialization: ${specialization.name}');
-//   }
-// }
-
-// class Nurse extends Staff {
-//   List<String> certification =[];
-//   int yearOfExperince;
-
-//   Nurse(
-//     String staffName,
-//     String gender,
-//     String dob,
-//     double baseSalary, 
-//     {this.yearOfExperince = 0}) : super (staffName, gender, dob, Role.Nurse, baseSalary);
-
-//     @override
-//   void printInfo() {
-//     super.printInfo();
-//     print('Certifications: ${certification.isEmpty ? "None" : certification.join(", ")}');
-//     print('Years of Experience: $yearOfExperince');
-//   }
-// }
-
-// class Admin extends Staff {
-//   String email;
-//   String password;
-
-//   Admin(
-//     String staffName,
-//     String gender,
-//     String dob,
-//     double baseSalary,
-//     this.email, 
-//     this.password) : super (staffName, gender, dob, Role.Admin, baseSalary);
-
-//     bool isAuthenticated (String inputEmail, String inputPassword) {
-//       return email == inputEmail && password == inputPassword;
-//     }
-
-//     @override
-//   void printInfo() {
-//     super.printInfo();
-//     print('Email: $email');
-//   }
-// }
